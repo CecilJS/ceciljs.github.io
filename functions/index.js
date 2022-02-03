5a8033f6e5972e3c92cd036f25ad9f0fd81879a7
Cecil Jones <dewdrops1800@outlook.com>
2022-02-03 13:32:37 +0000

    switched to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,12 @@
+const functions = require("firebase-functions");
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../app/build')));
+app.use(express.static(path.resolve(__dirname, '../build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -21,5 +15,10 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../app/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.app = functions.https.onRequest(app);
